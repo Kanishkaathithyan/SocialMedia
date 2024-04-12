0c970d0b7ee03b0dcc4b6db08f1a307554d0afd2
Kanishkaathithyan <163874272+Kanishkaathithyan@users.noreply.github.com>
2024-04-12 18:59:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import { Home } from "@material-ui/icons";
+//import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+// import Profile from "./pages/profile/Profile"
+// import Login from "./pages/login/Login";
+
+
+ function App() {
+  return (
+    <>
+    
+      <Profile/>
+
+    </>
+    
+  )  
+}
+
+export default App;
